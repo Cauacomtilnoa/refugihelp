9f2d4a31433b5497531b6eca0e4b1006cf7ac8bd
Cauacomtilnoa <126720628+Cauacomtilnoa@users.noreply.github.com>
2023-12-04 00:21:03 -0300

    aaaaa

--- src/pages/Voluntarios.js
+++ src/pages/Voluntarios.js
@@ -1,14 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col } from 'reactstrap';
-import voluntario from '../images/aaa.jpeg'
+import voluntario from '../images/aaa.jpg'
 
 function Voluntarios() {
     return (
-        <div className='p-0'>
-            <div className='img-fluid p-0' style={{ backgroundImage: `url(${voluntario})`, height: '60vh', backgroundSize: 'cover', backgroundPosition: 'center', marginTop: '-16px' }}>
-                <Row className='p-0 m-0'>
-                    <Col>
-                        <p></p>
+        <div className='p-0 m-0'>
+            <div className='img-fluid' style={{ backgroundImage: `url(${voluntario})`, height: '65vh', backgroundSize: 'cover', backgroundPosition: 'top', marginTop: '-16px' }}>
+                <Row className='p-0 m-0 h-100'>
+                    <Col className='text-white justify-content-center d-flex flex-column text-shadow'>
+                        <h1 className='display-4' style={{ textShadow: '2px 2px 5px black' }}>Seja um Voluntário na RefugiHelp</h1>
+                        <p className='lead fs-4' style={{ textShadow: '2px 2px 5px black' }}>Faça a diferença na vida dos refugiados. Junte-se à nossa equipe dedicada e contribua para um futuro mais brilhante.</p>
+                        <p className='lead fs-4' style={{ textShadow: '2px 2px 5px black' }}>Escolha entre diversas oportunidades de voluntariado e faça parte da mudança positiva que queremos ver no mundo.</p>
                     </Col>
                 </Row>
             </div>
